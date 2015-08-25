5c84f9e6d76c9e1eae64fed7908c323eacb3c9ba
okuda-natsumi <okuda.natsumi72@gmail.com>
2015-08-26 08:00:20 +0900

    4章p,118

---
+++ app/controllers/view_controller.rb
@@ -0,0 +1,32 @@
+class ViewController < ApplicationController
+	def form_tag
+		@book = Book.new
+	end
+
+	def field
+		@book = Book.new
+	end
+
+	def html5
+		@book = Book.new
+	end
+
+	def select
+		@book = Book.new(publish: '技術評論社')
+	end
+
+	def col_select
+		@book = Book.new(publish: '技術評論社')
+		@books = Book.select(:publish).distinct
+	end
+	
+	def col_select2
+		@book = Book.new(publish: '技術評論社')
+		@books = Book.select(:publish).distinct
+	end
+
+	def group_select
+		@review = Review.new
+		@authors = Author.all
+	end
+end
