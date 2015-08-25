9b96ed068d416de2b08c9c162f94445c7538ac62
okuda-natsumi <okuda.natsumi72@gmail.com>
2015-08-26 02:00:50 +0900

    2章終了

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,8 +1,24 @@
 # coding: utf-8
 class HelloController < ApplicationController
 
-def index
-  render text: 'こんにちは、世界！'
-end
+	def index
+	  render text: 'こんにちは、世界！'
+	end
+	
+	def view
+	  @msg = 'こんにちは、世界'
+	end
+	
+	def view2
+	  @msg = 'hello world'
+	end
+	
+	def list
+		@books = Book.all
+	end
+	
+	def app_var
+		render text: MY_APP['logo']['source']
+	end
 
 end
