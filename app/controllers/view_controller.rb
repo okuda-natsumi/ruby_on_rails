0d90b7dca304bbfd2c64064c5fd226a6731ab9a6
okuda-natsumi <okuda.natsumi72@gmail.com>
2015-08-28 17:55:13 +0900

    4章終了

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -51,4 +51,34 @@ class ViewController < ApplicationController
 	def conc
 		@books = Book.all
 	end
+
+  def adopt
+    render layout: 'sub'
+  end
+
+  def multi
+    render layout: 'layout'
+  end
+
+  def nest
+    @msg = '今日もいい天気ですよ。'
+    render layout: 'child'
+  end
+
+  def partial_basic
+    @book = Book.find(2)
+  end
+
+  def partial_param
+    @book = Book.find(2)
+  end
+
+  def partial_col
+    @books = Book.all
+  end
+
+  def partial_spacer
+    @books = Book.all
+  end
+
 end
